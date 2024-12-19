3b65cc96c1df96598d20d25d73eb2525041cda89
Tamara Bo Rabin <tr363@cornell.edu>
2024-12-18 22:21:27 -0500

    textures

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include "shader_s.h"
+
+#include "shaders/shader_s.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "textures/stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -10,41 +13,7 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
 
-// vertex shader
-// const char *vertexShaderSource1 = "#version 330 core\n"
-//                                   "layout (location = 0) in vec3 aPos;\n"
-//                                   "out vec4 vertexColor;\n"
-//                                   "void main()\n"
-//                                   "{\n"
-//                                   "   gl_Position = vec4(aPos, 1.0);\n"
-//                                   "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
-//                                   "}\0";
-// const char *vertexShaderSource2 = "#version 330 core\n"
-//                                   "layout (location = 0) in vec3 aPos;\n"
-//                                   "layout (location = 1) in vec3 aColor;\n"
-//                                   "out vec3 ourColor2;\n"
-//                                   "void main()\n"
-//                                   "{\n"
-//                                   "   gl_Position = vec4(aPos, 1.0);\n"
-//                                   "   ourColor2 = aColor;\n"
-//                                   "}\0";
-
-// fragment shader
-// const char *fragmentShaderSource1 = "#version 330 core\n"
-//                                     "out vec4 FragColor;\n"
-//                                     "uniform vec4 ourColor1;\n"
-//                                     "void main()\n"
-//                                     "{\n"
-//                                     "   FragColor = ourColor1;\n"
-//                                     "}\n\0";
-
-// const char *fragmentShaderSource2 = "#version 330 core\n"
-//                                     "out vec4 FragColor;\n"
-//                                     "in vec3 ourColor2;\n"
-//                                     "void main()\n"
-//                                     "{\n"
-//                                     "   FragColor = vec4(ourColor2, 1.0f);\n"
-//                                     "}\n\0";
+float mixValue = 0.2f;
 // main function to instantiate the GLFW window
 int main()
 {
@@ -83,70 +52,39 @@ int main()
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   // BUILD AND COMPILE SHADER PROGRAM
-
-  // VERTEX SHADER
-  // unsigned int vertexShader1 = glCreateShader(GL_VERTEX_SHADER);
-  // // attach the shader source code to the shader object & compile the shader
-  // glShaderSource(vertexShader1, 1, &vertexShaderSource1, NULL);
-  // glCompileShader(vertexShader1);
-
-  // unsigned int vertexShader2 = glCreateShader(GL_VERTEX_SHADER);
-  // // attach the shader source code to the shader object & compile the shader
-  // glShaderSource(vertexShader2, 1, &vertexShaderSource2, NULL);
-  // glCompileShader(vertexShader2);
-
-  // // FRAGMENT SHADER
-  // // fragment shader 1
-  // unsigned int fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
-  // glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, NULL);
-  // glCompileShader(fragmentShader1);
-  // // fragment shader 2
-  // unsigned int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
-  // glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
-  // glCompileShader(fragmentShader2);
-
-  // link shaders
-  // unsigned int shaderProgram1 = glCreateProgram();
-  // unsigned int shaderProgram2 = glCreateProgram();
-  // // attach shaders to the program object
-  // glAttachShader(shaderProgram1, vertexShader1);
-  // glAttachShader(shaderProgram1, fragmentShader1);
-  // glLinkProgram(shaderProgram1);
-
-  // glAttachShader(shaderProgram2, vertexShader2);
-  // glAttachShader(shaderProgram2, fragmentShader2);
-  // glLinkProgram(shaderProgram2);
-  // // check for compilation
-
-  // glDeleteShader(vertexShader1);
-  // glDeleteShader(vertexShader2);
-  // glDeleteShader(fragmentShader1);
-  // glDeleteShader(fragmentShader2);
-  Shader shader1("vertexShader1.vs", "fragShader1.fs");
-  Shader shader2("vertexShader2.vs", "fragShader2.fs");
+  Shader shader1("shaders/vertexShader1.vs", "shaders/fragShader1.fs");
+  Shader shader2("shaders/vertexShader2.vs", "shaders/fragShader2.fs");
+  Shader shader3("shaders/vertexShader3.vs", "shaders/fragShader3.fs");
 
   // TRIANGLE VERTICES
   float vertices1[] = {
-      -0.55f, 0.3f, 0.0f, // top
-      -0.3f, -0.3f, 0.0f, // bottom right
-      -0.8f, -0.3f, 0.0f, // bottom left
-
+      -0.55f, 0.0f, 0.0f, // top
+      -0.3f, -0.4f, 0.0f, // bottom right
+      -0.8f, -0.4f, 0.0f, // bottom left
   };
 
   float vertices2[] = {
-      // positions       //colors
-      0.55f, 0.3f, 0.0f, 1.0f, 0.0f, 0.0f, // top
-      0.3f, -0.3f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
-      0.8f, -0.3f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+      // positions       // colors
+      0.55f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // top
+      0.3f, -0.4f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      0.8f, -0.4f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
   };
-  // unsigned int indices[] = {aa
-  //     0, 1, 3,
-  //     1, 2, 3};
-  unsigned int VBOs[2], VAOs[2];
-  glGenVertexArrays(2, VAOs);
+
+  float vertices3[] = {
+      // positions      // colors         // texture
+      0.4f, 0.8f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
+      0.4f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -0.4f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.4f, 0.8f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3,
+      1, 2, 3};
+  unsigned int VBOs[3], VAOs[3], EBO;
+  glGenVertexArrays(3, VAOs);
   // create vertex buffer object VBO
-  glGenBuffers(2, VBOs);
-  // glGenBuffers(1, &EBO);
+  glGenBuffers(3, VBOs);
+  glGenBuffers(1, &EBO);
   // 1. bind vertex array object
 
   //  2. copy our vertices array in a buffer for OpenGL to use
@@ -171,11 +109,75 @@ int main()
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
 
+  // square
+  glBindVertexArray(VAOs[2]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBOs[2]);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices3), vertices3, GL_STATIC_DRAW);
+  // vertex attributes
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  // color attributes
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // texture attributes
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
 
   // draw in wireframe polygons
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  // get texture for square
+  unsigned int textures[2];
+  glGenTextures(2, textures);
+  glBindTexture(GL_TEXTURE_2D, textures[0]);
+  // set the texture wrapping.filtering options
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load and generate texture
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true);
+  // load container
+  unsigned char *data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);
+  if (data)
+  {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+  // load face
+  glBindTexture(GL_TEXTURE_2D, textures[1]);
+  // set the texture wrapping.filtering options
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  data = stbi_load("textures/awesomeface.png", &width, &height, &nrChannels, 0);
+  if (data)
+  {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  // set the textures to shader uniforms
+  shader3.use();
+  shader3.setInt("texture1", 0);
+  shader3.setInt("texture2", 1);
 
   // RENDER LOOP
   while (!glfwWindowShouldClose(window))
@@ -207,6 +209,16 @@ int main()
     glDrawArrays(GL_TRIANGLES, 0, 3);
     // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+    // draw rectangle
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+    shader3.use();
+    shader3.setFloat("mixValue", mixValue);
+    // float mixValue = sin(timeValue) / 2.0f + 0.5f;
+    glBindVertexArray(VAOs[2]);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // check and call events and swap the buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -216,8 +228,6 @@ int main()
   glDeleteVertexArrays(2, VAOs);
   glDeleteBuffers(2, VBOs);
   // glDeleteBuffers(1, &EBO);
-  // glDeleteProgram(shaderProgram1);
-  // glDeleteProgram(shaderProgram2);
 
   // clean/delete all GLFW's resources that were allocated
   glfwTerminate();
@@ -235,5 +245,18 @@ void processInput(GLFWwindow *window)
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
   {
     glfwSetWindowShouldClose(window, true);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+  {
+    mixValue += 0.005f;
+    if (mixValue >= 1.0f)
+      mixValue = 1.0f;
+  }
+  if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+  {
+    mixValue -= 0.005f;
+    if (mixValue <= 0.0f)
+      mixValue = 0.0f;
   }
 }
